a0ee29c03d4b60ad49c655a1ee91362a6d29f975
prabhatpk363-bit <prabhatpk363@gmail.com>
2026-08-13 16:59:32 +0530

    newly run

--- Environments/prod/provider.tf
+++ Environments/prod/provider.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "prod-pk"
     storage_account_name = "pkstorage1231"
     container_name       = "pkcontainer1"
-    key                  = "pk1.tfstate"
+    key                  = "pk12.tfstate"
 
   }
 }
